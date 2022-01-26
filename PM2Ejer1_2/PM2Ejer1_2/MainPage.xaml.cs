5f8c9aa12d3bc21b9dc967b119297f5260756d98
danie <danie@DANIEL-GB>
2022-01-26 00:16:31 -0600

    Add project files.

---
+++ PM2Ejer1_2/PM2Ejer1_2/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PM2Ejer1_2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async private void btnpagina_Clicked(object sender, EventArgs e)
+        {
+            var Contact = new Models.Contactos
+            {
+                Nombre = this.txtnombre.Text,
+                Apellidos = this.txtapellidos.Text,
+                Edad = Convert.ToInt32(this.txtedad.Text),
+                Email = this.txtemail.Text
+            };
+            var pagina = new PageMain();
+            pagina.BindingContext = Contact;
+            await Navigation.PushModalAsync(pagina);
+        }
+    }
+}
+
---
+++ PM2Ejer1_2/PM2Ejer1_2/Models/Contactos.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PM2Ejer1_2.Models
+{
+    class Contactos
+    {
+        public String Nombre { get; set; }
+        public String Apellidos { get; set; }
+        public Int32 Edad { get; set; }
+        public String Email { get; set; }
+    }
+}
